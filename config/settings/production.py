c48cbe1ec0658047571333eff01497edae9a79f4
Tadej Svetina <tadej.svetina@gmail.com>
2018-01-05 20:22:51 +0100

    test

--- config/settings/production.py
+++ config/settings/production.py
@@ -62,7 +62,9 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['p-rationality.com', ])
+#ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['p-rationality.com', ])
+ALLOWED_HOSTS = ['18.195.151.190', ]
+
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
